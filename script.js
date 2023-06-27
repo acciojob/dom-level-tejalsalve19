3a30a7f2006377bb1e4f4d70ea30d76edcd8a075
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-27 16:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.getElementById("level");
+  const level = calculateDOMLevel(element);
+  alert("The level of the element is: " + level);
+});
+
+function calculateDOMLevel(element) {
+  let level = 0;
+  let currentElement = element;
+
+  while (currentElement.parentNode !== null) {
+    currentElement = currentElement.parentNode;
+    level++;
+  }
+
+  return level;
+}
